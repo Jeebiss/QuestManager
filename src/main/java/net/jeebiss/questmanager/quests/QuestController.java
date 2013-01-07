12b72a8e94a272d035a0cf9dbe292d397516c9b8
Mason Adkins <zappalives0@gmail.com>
2013-01-06 23:25:22 -0500

    started Chapter handling

--- src/main/java/net/jeebiss/questmanager/quests/QuestController.java
+++ src/main/java/net/jeebiss/questmanager/quests/QuestController.java
@@ -1,5 +1,6 @@
 package net.jeebiss.questmanager.quests;
 
+import java.util.List;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
@@ -10,7 +11,12 @@ import net.aufdemrand.denizen.utilities.debugging.dB;
 public class QuestController {
 
 	Denizen denizen = (Denizen) Bukkit.getPluginManager().getPlugin("Denizen");
+	
 	Set<String> chapters;
+	List<String> requirements;
+	
+	String playerChapter = null;
+	
 	
 	public QuestController(String scriptName, String questName) {
 		dB.echoDebug("Creating a controller for " + scriptName + " as " + questName);
@@ -20,6 +26,24 @@ public class QuestController {
 				&& denizen.getScripts().contains(scriptName.toUpperCase() + ".CHAPTERS")) {
 			chapters = denizen.getScripts()
 					.getConfigurationSection(scriptName.toUpperCase() + ".CHAPTERS").getKeys(false);
+		} else  {
+			dB.echoDebug("...no Chapters: node found");
+			return;
 		}
+		
+		playerChapter = findChapter(scriptName);
+	}
+	
+	public String findChapter(String scriptName) {
+		String currentChapter = null;
+		
+		//parse over chapters
+		for (String chapter : chapters) {
+			if (denizen.getScripts().getString(scriptName.toUpperCase() + "." + chapter + ".REQUIREMENTS") != null) {
+				requirements = denizen.getScripts().getStringList(scriptName.toUpperCase() + "." + chapter + ".REQUIREMENTS");
+			} else currentChapter = chapter;
+		}
+		
+		return currentChapter;
 	}
 }
