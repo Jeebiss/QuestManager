8865e4f16b68d7a15440f56364d22bfef882d737
Mason Adkins <zappalives0@gmail.com>
2013-01-06 21:36:04 -0500

    POM changes. added debug

--- src/main/java/net/jeebiss/questmanager/quests/QuestController.java
+++ src/main/java/net/jeebiss/questmanager/quests/QuestController.java
@@ -1,8 +1,19 @@
 package net.jeebiss.questmanager.quests;
 
+import java.util.Set;
+
+import org.bukkit.Bukkit;
+
+import net.aufdemrand.denizen.Denizen;
+import net.aufdemrand.denizen.utilities.debugging.dB;
+
 public class QuestController {
+
+	Denizen denizen = (Denizen) Bukkit.getPluginManager().getPlugin("Denizen");
+	
 	public QuestController(String scriptName) {
-		
+		dB.echoDebug("Creating a controller for: " + scriptName);
+		//denizen.getScripts();
 	}
 	
 }
