2c10ebfa0103e57b835d3a283d84ae0d7e36cab9
Mason Adkins <zappalives0@gmail.com>
2013-01-06 22:53:29 -0500

    gets Set of Chapters: from scriptName

--- src/main/java/net/jeebiss/questmanager/quests/QuestController.java
+++ src/main/java/net/jeebiss/questmanager/quests/QuestController.java
@@ -10,10 +10,16 @@ import net.aufdemrand.denizen.utilities.debugging.dB;
 public class QuestController {
 
 	Denizen denizen = (Denizen) Bukkit.getPluginManager().getPlugin("Denizen");
+	Set<String> chapters;
 	
 	public QuestController(String scriptName, String questName) {
 		dB.echoDebug("Creating a controller for " + scriptName + " as " + questName);
-		//denizen.getScripts();
+		
+		//get list of chapters
+		if (denizen.getScripts().getString(scriptName.toUpperCase() + ".CHAPTERS") != null 
+				&& denizen.getScripts().contains(scriptName.toUpperCase() + ".CHAPTERS")) {
+			chapters = denizen.getScripts()
+					.getConfigurationSection(scriptName.toUpperCase() + ".CHAPTERS").getKeys(false);
+		}
 	}
-	
 }
