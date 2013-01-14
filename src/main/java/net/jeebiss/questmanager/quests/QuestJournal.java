1b29e1589789d3068aa8019e0ab00f75ee52e7f7
dbixler <drbixler@gmail.com>
2013-01-13 19:17:40 -0500

    Added commands to determine if a player has started a quest and one for if they have finished a quest.

--- src/main/java/net/jeebiss/questmanager/quests/QuestJournal.java
+++ src/main/java/net/jeebiss/questmanager/quests/QuestJournal.java
@@ -36,4 +36,36 @@ public class QuestJournal {
 		this.quests.put (questName, quest);
 		return quest;
 	}
+	
+	/**
+	 * Returns whether or not a player has started a particular quest chapter.
+	 * 
+	 * @param questName	The name of the quest
+	 * @param chapterName	The chapter name to check.
+	 * 
+	 * @return	True if the quest and quest chapter have been started.
+	 */
+	public boolean isQuestStarted (String questName, String chapterName) {
+		if (this.quests.containsKey(questName)		&&
+				 this.quests.get(questName).getChapter(chapterName) != null) {
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns whether or not a player has completed a particular quest chapter.
+	 * 
+	 * @param questName	The name of the quest.
+	 * @param chapterName	The chapter name of the quest.
+	 * 
+	 * @return	True if the player has completed the quest, false otherwise.
+	 */
+	public boolean isQuestFinshed (String questName, String chapterName) {
+		if (this.quests.containsKey(questName)		&&
+				this.quests.get(questName).getChapter(chapterName) != null) {
+			return this.quests.get(questName).getChapter (chapterName).isComplete();
+		}
+		return false;
+	}
 }
