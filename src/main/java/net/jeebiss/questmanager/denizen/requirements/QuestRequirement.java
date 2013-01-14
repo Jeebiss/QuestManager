9ea81bca67159f268112aa08dfba670466a6120b
dbixler <drbixler@gmail.com>
2013-01-13 20:29:29 -0500

    Cleaned up the QuestRequirement requirement class.

--- src/main/java/net/jeebiss/questmanager/denizen/requirements/QuestRequirement.java
+++ src/main/java/net/jeebiss/questmanager/denizen/requirements/QuestRequirement.java
@@ -11,79 +11,93 @@ import net.aufdemrand.denizen.scripts.requirements.AbstractRequirement;
 import net.aufdemrand.denizen.utilities.arguments.aH;
 import net.aufdemrand.denizen.utilities.debugging.dB;
 import net.jeebiss.questmanager.QuestManager;
+import net.jeebiss.questmanager.quests.QuestChapter;
+import net.jeebiss.questmanager.quests.QuestJournal;
+import net.jeebiss.questmanager.quests.QuestChapter.Status;
 
 public class QuestRequirement extends AbstractRequirement{
 	private enum ScriptType { FINISHED, STARTED, FAILED }
 	@Override
 	public boolean check(Player player, DenizenNPC npc, String scriptName,
 			List<String> args) throws RequirementCheckException {
-		
+
 		// Requirement will check if full Quest, or specific
 		// Quest Chapter is FINISHED, STARTED, or FAILED and
 		// will be compatible with the denizen scripts. Using
 		// a hyphen will denote that the requirement NOT be met.
 		
 		QuestManager QM = (QuestManager) Bukkit.getServer().getPluginManager().getPlugin("Quest Manager");
+
+		ScriptType scriptType = ScriptType.FINISHED;
 		
-		ScriptType TYPE = ScriptType.FINISHED;
-		
-		Boolean outcome = false;
 		Integer quantity = 1;
-		String[] path;
-		String quest = null;
-		String chapter = null;
-		String playerName = player.getName();
-		String questStatus = null;
-		String chapterStatus = null;
+		String questName = null;
+		String chapterName = null;
 		
 		for (String arg : args) {
 			if (aH.matchesArg("FINISHED, STARTED, FAILED", arg)) {
 				try {
-					TYPE = ScriptType.valueOf(aH.getStringFrom(arg));
-					dB.echoDebug("...set TYPE to: " + TYPE.name());
+					scriptType = ScriptType.valueOf(aH.getStringFrom(arg));
+					dB.echoDebug("...set TYPE to: " + scriptType.name());
 				} catch (Exception e) {e.printStackTrace();}
 			} else if (aH.matchesQuantity(arg)) {
 				quantity = aH.getIntegerFrom(arg);
 				dB.echoDebug("...set quantity to: " + quantity);
 			} else {
 				if (arg.contains(".")) {
-					path = arg.split("\\.");
-					quest = path[0];
-					chapter = path[1];
-				} else quest = arg;
+					String[] path = arg.split("\\.");
+					questName = path[0];
+					chapterName = path[1];
+				} else {
+					questName = arg;
+				}
 			}
-		
 		}
 		
-		// if the quest doesnt exist in the Player's
-		// node in the saves.yml, its neither FINISHED,
-		// FAILED, or STARTED.
+		//
+		// Fetch the player's Quest Journal.
+		//
+		QuestJournal	qj = QM.getQuestJournal(player);
 		
-		if (QM.getSaves().contains("Quests." + playerName + "." + quest)) {
-			//if 'quest' exists, then it has a Status:
-			questStatus = QM.getSaves().getString("Quests." + playerName + "." + quest + ".Status");
-			
-			//if 'chapter' is null, then the user only require the 'quest' status
-			if (chapter == null) {
-				//if 'questStatus' is equal to the type of check
-				// then the requirement is met, and return the
-				// outcome of the requirement
-				if (questStatus == TYPE.name()) outcome = true;
-				return outcome;
-			// If the chapter exists, then it has a status to check
-			} else if (QM.getSaves().contains("Quests." + playerName + "." + quest + "." + chapter)) {
-				// if 'chapter' exists, then it has a Status:
-				chapterStatus = QM.getSaves().getString("Quests." + playerName + "." + quest + "." + chapter + ".Status");
-				
-				// if the 'chapterStatus' is equal to the type of
-				// check, then the requirement is met. Then return
-				// the outcome.
-				if (chapterStatus == TYPE.name()) outcome = true;
-			} 
-			
-		} else outcome = false;
+		//
+		// If the quest journal does not have the quest, then the quest is not
+		// started.
+		//
+		if (qj.hasQuest(questName) == false) {
+			return false;
+		}
 		
-		return outcome;
+		//
+		// If there is no chapter, but we have the quest (which would be odd), then
+		// we'll assume that the quest has been started.  This would only return
+		// true if it's checked for being started, otherwise, this returns false.
+		//
+		if (chapterName == null) {
+			return scriptType == ScriptType.STARTED;
+		}
+		
+		//
+		// If the chapter does not exist, then the quest isn't started.
+		//
+		QuestChapter	qc = qj.getQuests().get(questName).getChapter(chapterName);
+		if (qc == null) {
+			return false;
+		}
+
+		//
+		// So the quest is "at least" in a "STARTED" state.  Return based on what
+		// the script wants.
+		//
+		switch (scriptType) {
+		case STARTED:
+			return true;
+		case FINISHED:
+			return qc.getStatus() == Status.FINISHED;
+		case FAILED:
+			return qc.getStatus () == Status.FAILED;
+		}
+		
+		return false;
 	}
 
 }
