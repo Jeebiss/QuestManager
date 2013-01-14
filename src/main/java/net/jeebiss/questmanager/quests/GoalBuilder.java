200cd6b55524c477decb9e7542772d9f79d0b215
dbixler <drbixler@gmail.com>
2013-01-13 23:47:48 -0500

    Cleaned up GoalBuilder.

--- src/main/java/net/jeebiss/questmanager/quests/GoalBuilder.java
+++ src/main/java/net/jeebiss/questmanager/quests/GoalBuilder.java
@@ -1,47 +1,41 @@
 package net.jeebiss.questmanager.quests;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import net.aufdemrand.denizen.Denizen;
 import net.aufdemrand.denizen.exceptions.ScriptEntryCreationException;
 import net.aufdemrand.denizen.scripts.ScriptBuilder;
-import net.aufdemrand.denizen.scripts.ScriptEngine.QueueType;
 import net.aufdemrand.denizen.scripts.ScriptEntry;
+import net.aufdemrand.denizen.scripts.commands.CommandExecuter;
 import net.aufdemrand.denizen.utilities.DenizenAPI;
 
 import org.bukkit.entity.Player;
 
+/**
+ * This is a utility class to build a list of goals used by Quest Manager to
+ * determine when a player has completed a quest chapter.
+ * 
+ * @author Jeebis
+ */
 public class GoalBuilder {
-	
-	String listenerType;
-	String[] args;
-	
-	ScriptEntry scriptEntry;
-	ScriptBuilder scriptBuilder = DenizenAPI.getCurrentInstance().getScriptEngine().getScriptBuilder();
-	
 	public GoalBuilder(Player player, List<String> commands) {
-		// will parse list of goals, and build the proper listeners
+		ScriptBuilder scriptBuilder = DenizenAPI.getCurrentInstance().getScriptEngine().getScriptBuilder();
+		CommandExecuter	executor = DenizenAPI.getCurrentInstance().getScriptEngine().getScriptExecuter();
+		String[] args;
+
+		//
+		// For each goal, build the proper listener.
+		//
 		for (String goal : commands) {
-			// Break the Goal into arguments for the
-			// LISTEN command scriptEntry
-			args = scriptBuilder.buildArgs(player, null, goal);
-			
-			// Build neew scriptEntry with the args
+			//
+			// Break the Goal into arguments for the LISTEN command scriptEntry and
+			// execute the script.
+			//
 			try {
-				scriptEntry = new ScriptEntry("LISTEN", args, player);
+				args = scriptBuilder.buildArgs(player, null, goal);
+				executor.execute(new ScriptEntry("LISTEN", args, player));
 			} catch (ScriptEntryCreationException e) {
 				e.printStackTrace();
 			}
-				
-			/*
-			 * Ues build scriptEntry to build the
-			 * listener for the goal.
-			 */
-		
-			DenizenAPI.getCurrentInstance().getScriptEngine().getScriptExecuter().execute(scriptEntry);
-
 		}
 	}
-
 }
